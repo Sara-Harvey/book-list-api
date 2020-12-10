85fa0014f36a8f6aa8e6ee98f948836344625295
Sara Harvey <saraharveynyc@gmail.com>
2020-12-10 12:23:20 -0500

    added more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,6 @@ comic = Category.create(name: "comic")
 fiction = Category.create(name: "fiction")
 nonfiction = Category.create(name: "nonfiction")
 
-first_book = Book.create(date: "2009-09-01", name: "Lucky", author: "Gabrielle Bell", note: "Cool self-published 2006 debut", category_id: comic.id)
+first_book = Book.create(date: "2020-09-01", name: "Lucky", author: "Gabrielle Bell", note: "Cool self-published 2006 debut", category_id: comic.id)
+second_book = Book.create(date: "2020-10-01", name: "Total Cat Mojo", author: "Jackson Galaxy", note: "We're going to raise this foster kitten right", category_id: nonfiction.id)
+first_book = Book.create(date: "2020-11-01", name: "Boys of Alabama", author: "Genevieve Hudson", note: "Memories of the South", category_id: fiction.id)
