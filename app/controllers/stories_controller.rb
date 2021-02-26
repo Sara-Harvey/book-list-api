8b33ca3672d6dcfa7d020a4816c2dd1cbcc0a9fd
Sara Harvey <saraharveynyc@gmail.com>
2021-02-26 09:45:37 -0500

    added create, destroy and update functions to stories_controller

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,10 +1,10 @@
 class StoriesController < ApplicationController
-	
-	def index
-		stories = Story.all 
-		options = {	include: [:category] }
-		render json: StorySerializer.new(stories, options) 
-	end
+  
+  def index
+    stories = Story.all 
+    options = { include: [:category] }
+    render json: StorySerializer.new(stories, options) 
+  end
 
     def show
       story = Story.find_by(id: params[:id])
@@ -18,19 +18,36 @@ class StoriesController < ApplicationController
         end
     end
 
-	def create
-		story = Story.new(story_params) 
-		if story.save
-			options = { include: [:category] }
-			render json: StorySerializer.new(story, options), status: :accepted
-		else
-			render json: {errors: story.errors.full_messages}, status: :unprocessable_entity 
-		end
-	end 
+  def create
+    story = Story.new(story_params) 
+    if story.save
+      options = { include: [:category] }
+      render json: StorySerializer.new(story, options), status: :accepted
+    else
+      render json: {errors: story.errors.full_messages}, status: :unprocessable_entity 
+    end
+  end
 
-	private
-	def story_params
-			params.require(:story).permit(:name, :description, :link, :category_id)
-		
-	end
+  def destroy
+    story = Story.find(params[:id])
+    story.destroy
+    render json: story
+  end
+
+  def update 
+    story = Story.find(params[:id])
+    story.update(story_params)
+      if story.save    
+        render json: story, status: 200
+      else
+        render json: { errors: story.errors.full_messages }, 
+        status: :unprocessible_entity
+      end
+  end
+
+  private
+  def story_params
+      params.require(:story).permit(:name, :description, :link, :category_id)
+    
+  end
 end
